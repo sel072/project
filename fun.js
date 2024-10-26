1686ddb5bc3163549ba07b0a419337ed7b3d59fe
sel072 <lokeshwaran05021997@gmail.com>
2024-10-26 10:00:32 -0700

    Add files via upload

---
+++ fun.js
@@ -0,0 +1,91 @@
+function run() {
+      var s=document.getElementById("zero").value
+      if (s=='Tamil')
+      {
+      	window.location.href="./index3.html"
+      }
+       if (s=='Hindi')
+      {
+            window.location.href="./index92.html"
+      }
+       if (s=='Kannada')
+      {
+            window.location.href="./index93.html"
+      }
+}
+function non() {
+ window.location.href="./index2.html"
+}
+
+function t() {
+ window.location.href="./index1.html"
+}
+
+function c() {
+ window.location.href="./index2.html"
+}
+function don() {
+ window.location.href="./index92.html"
+}
+function won() {
+ window.location.href="./index93.html"
+}
+function ton() {
+ window.location.href="./index1.html"
+}
+//confirm password//
+  function confirmpassword(){
+                  
+            var data=document.getElementById("van").value
+            var gg=document.getElementById("han").value
+    
+            if(data==gg)
+            {
+                  alert ("both passwords are similar!")
+                  window.location.href="index38.html"
+            }
+            else{
+                  alert("both passwords are not similar!")
+            }
+       }
+  
+
+
+
+
+
+        
+
+     
+      
+     
+            
+
+
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
